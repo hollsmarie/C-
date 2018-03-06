abe3881fdc3c310b3508f51e9f3a8549715c0373
Nicholas De Fina <nicholasdefina@gmail.com>
2018-03-06 16:41:39 -0600

    initial commit

---
+++ Basic13/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace Basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //print 1-255
+
+            // for (int i = 1; i < 256; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+//------------------------------------------------------------------
+
+            //print odd 1-255
+
+            // for (int i = 1; i < 256; i++)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // }
+//------------------------------------------------------------------
+
+            //print sum
+
+            // int sum = new int();
+            // for (int i = 0; i <256; i++)
+            // {
+            //     sum += i;
+            //     Console.WriteLine("New number: " +i+ "Sum: " + sum);
+            // }
+
+  //------------------------------------------------------------------          
+
+            //iterate thrugh array
+
+            // int [] newArray = {1,3,5,7,9,13};
+
+            // foreach (int number in newArray)
+            // {
+            //     Console.WriteLine(number);
+            // }
+
+//------------------------------------------------------------------
+
+
+            //find max
+
+            // int [] maxArray = {-4,58,9,0,13};
+            // int max = maxArray[0];
+
+            // foreach (int numb in maxArray)
+            // {
+            //     if (numb > max)
+            //     {
+            //         max = numb;
+            //     }
+            // }
+            // Console.WriteLine(max);
+
+//------------------------------------------------------------------
+
+            //get average
+
+            // int [] avgArray = {2,10,3};
+            // int sum = new int();
+
+            // foreach (int numero in avgArray)
+            // {
+            //     sum += (int)numero;
+
+            // }
+            // int avg = sum/avgArray.Length;
+            // Console.WriteLine(avg);
+
+//------------------------------------------------------------------
+
+            //array with odd numbers
+
+            // List<int> tempList = new List<int>();
+
+            // for (int i = 1; i <256; i++)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         tempList.Add(i);
+            //     }
+            // }
+            // int [] Y =  tempList.ToArray();
+            // Console.WriteLine(Y);
+
+//------------------------------------------------------------------
+
+
+            //greater than Y
+
+            // int Y = 3;
+            // int[] thisarray = {1,3,5,7};
+            // int count = 0;
+
+            // for (int i = 0; i < thisarray.Length; i++)
+            // {
+            //     if (thisarray[i] > Y)
+            //     {
+            //         count++;
+            //     }
+            // }
+            // System.Console.WriteLine(count);
+
+//------------------------------------------------------------------
+
+
+            //square the values
+
+            // int [] x = {1,5,10,-2};
+
+            // for (int i = 0; i < x.Length; i++)
+            // {
+            //     x[i] = x[i] * x[i];
+            //     System.Console.WriteLine(x[i]);
+            // }
+        
+
+//------------------------------------------------------------------
+
+            // eliminate negative numbers
+
+            // int [] x = {1,5,10,-2};
+
+            // for (int i = 0; i < x.Length; i++)
+            // {
+            //     if (x[i] < 0)
+            //     {
+            //         x[i]=0;
+            //     }
+            // }
+            // System.Console.WriteLine(x);
+
+
+
+//___________________________________________________
+
+            //min,max,avg
+
+            // int[] x = {1,5,10,-2};
+            // int max = x[0];
+            // int min = x[0];
+            // int sum = new int();
+
+            // for (int i = 0; i < x.Length; i++)
+            // {
+            //     if (max < x[i])
+            //     {
+            //         max = x[i];
+            //     }
+
+            //     if (min > x[i])
+            //     {
+            //         min = x[i];
+            //     }
+
+            //     sum = sum + x[i];
+
+            // }
+
+            // int avg = sum/x.Length;
+
+            // int [] mmaArray = {max, min, avg};
+            
+            // System.Console.WriteLine(mmaArray);
+
+
+//----------------------------------------------------
+
+
+            //shifting the values in an array
+
+            // int [] x = {1,5,10,7,-2};
+
+            // for (int i = 0; i < x.Length; i++)
+            // {
+            //     if (x[i] == x[x.Length-1])
+            //     {
+            //         x[i] = 0;
+            //     }
+            //     else
+            //     {
+            //     x[i] = x[i+1];
+            //     }
+                
+            // }
+
+            // System.Console.WriteLine(x);
+
+
+
+            // number to string UNFINISHED
+
+            // int [] x = {-1,-3,2};
+            // string dojo = "Dojo";
+
+            // for (int i = 0; i < x.Length; i++)
+            // {
+            //     if (x[i] < 0)
+            //     {
+            //         x[i]= "Dojo";
+            //     }
+            // }
+            // System.Console.WriteLine(x);
+
+        }
+
+    }
+}
---
+++ Puzzles/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Puzzles
+{
+    class Program
+    {
+
+        public static void RandomArray()
+        {
+
+            Random rand = new Random();
+            int [] randarray = new int [10];
+            int sum = new int();
+            
+            for (int i = 0; i <10; i++)
+            {
+
+                randarray[i] = rand.Next(5,26);
+                sum+=randarray[i];
+
+            }
+            int max = randarray[0];
+            int min = randarray[0];
+
+            for (int j = 0; j<10;j++)
+            {
+                if ( max < randarray[j])
+                {
+                    max = randarray[j];
+                }
+                if ( min > randarray[j])
+                {
+                    min = randarray[j];
+                }
+            }
+
+            System.Console.WriteLine(randarray);
+            System.Console.WriteLine(max);
+            System.Console.WriteLine(min);
+            System.Console.WriteLine(sum);
+        }
+
+        
+
+        public static string CoinFlip()
+        {
+            System.Console.WriteLine("Tossing a coin...");
+            Random flip = new Random ();
+            int results = flip.Next(1,3);
+            string tossResult = "";
+
+            if (results == 1)
+            {
+                 tossResult = "It's a heads.";
+            }
+            else{
+                 tossResult = "It's a tails.";
+            }
+            
+
+            return tossResult;
+
+
+        }
+
+        public static double TossMultipleCoins(int num)
+        {
+            string toss;
+            double sumhead = 0;
+            double sumtail = 0; 
+            for (int i =0; i < num; i++)
+            {
+                toss = CoinFlip();
+                System.Console.WriteLine(toss);
+                if (toss == "It's a heads.")
+                {
+                    sumhead+=1;
+                }
+                if (toss == "It's a tails.")
+                {
+                    sumtail+=1;
+                }
+                
+            }
+                double result = sumhead/sumtail;
+                System.Console.WriteLine(result);
+                return result;
+        }
+
+
+        public static string[] Names()
+        {
+            string [] newArray = {"Todd", "Tiffany", "Charlie", "Geneva", "Sydney"};
+            Random rand = new Random();
+            string holder = "";
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                int index = rand.Next(i, newArray.Length);
+                holder = newArray[i];
+                newArray[i] = newArray[index];
+                newArray[index]=holder;
+            }
+            
+            System.Console.WriteLine(newArray);
+            List<string> longName = new List<string>();
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (newArray[i].Length > 5)
+                {
+                    longName.Add(newArray[i]);
+                }
+            }
+
+
+            string[] longNames = longName.ToArray();
+            for (int i = 0; i < longNames.Length; i++)
+            {
+                System.Console.WriteLine(longNames[i]);
+            }
+
+            
+            return newArray;
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            // RandomArray();
+
+//-----------------------------------------------------------
+
+
+            // string toss;
+            // toss=CoinFlip();
+            // System.Console.WriteLine(toss);
+
+
+//-----------------------------------------------------------
+
+            // int tossnumber = 10;
+            // double ratio = TossMultipleCoins(tossnumber);
+            // System.Console.WriteLine("The ratio of heads to tails is {0}", ratio.ToString());
+
+
+//-----------------------------------------------------------
+
+
+            Names();
+
+        }
+    }
+}
