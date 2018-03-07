db1351008f92ef8e0fe09951a7b50f75622c498c
Nicholas De Fina <nicholasdefina@gmail.com>
2018-03-07 17:20:23 -0600

    updated assignments

--- DeckOfCards/Deck.cs
+++ DeckOfCards/Deck.cs
@@ -8,10 +8,57 @@ namespace DeckOfCards
     public class Deck
     {
 
-        public List<Card> cards = new List<Card>();
+        public List<Card> cards = new List<Card>(); //making a list of the card makes it refer to the object Card
+
+        public Deck reset()
+        {
+            string[] suits = { "Diamonds", "Hearts", "Clubs", "Spades" };
+            string[] stringVals = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            foreach (string suit in suits)
+            {
+                for (int i = 0; i < stringVals.Length; i++)
+                {
+                    Card newCard = new Card(suit, stringVals[i], i++);
+                    cards.Add(newCard);  //add to the list of cards created above
+
+                }
+            }
+            return this;
+        }
+
+        public Deck deal()
+        {
+            if(cards.Count > 0)
+            {
+                Card first = cards[0];
+                cards.RemoveAt(0);
+                return first;
+            }
+            else{
+                reset();
+                return deal();
+            }
+
+        }
 
 
+        public void shuffle()
+        {
+            
+            Random rand = new Random();
+            for (int end=cards.Count - 1; end>=0; end --)
+            {
+                int shuffle = rand.Next(0, cards.Count-1);
+
+                Card temp = cards[shuffle];
+                cards[shuffle] = cards[end];
+                cards[end] = temp;
+            }
+
+
+
+        }
     }
 
-    
 }
--- DeckOfCards/Player.cs
+++ DeckOfCards/Player.cs
@@ -1,8 +1,41 @@
 using System;
 using System.Collections.Generic;
 namespace DeckOfCards
-{
 
+
+{
+    public class Player
+    {
+        string name; //player has a name attribute
+        public List<Card> hand; //player has hand property 
+
+        public Player(string person)
+        {
+            name = person;
+            hand = new List<Card>();
+        }
+
+        public Card draw (Deck addhand)
+
+        {
+            Card newCard = addhand.deal();
+            hand.Add(newCard);
+            return newCard;
+        }
+
+        public Card discard (int idx)
+        {
+            if (idx < 0 || idx > hand.Count){
+                return null;
+            }
+            else 
+            {
+                Card shuffle = hand[idx];
+                hand.RemoveAt(idx);
+                return shuffle;
+            }
+        }
+    }
 
 
     
